4fa6326043e5b45349392a5147903ab006752a3a
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2024-11-05 12:12:20 +0000

    ***Added*** - documentation to PushFacade

--- simple_repositories/src/main/java/com/adrian/simple_repositories/facade/PushFacade.java
+++ simple_repositories/src/main/java/com/adrian/simple_repositories/facade/PushFacade.java
@@ -41,6 +41,17 @@ import com.adrian.simple_repositories.service.ProjectService;
 import com.adrian.simple_repositories.service.UserProjectActivityService;
 import com.adrian.simple_repositories.service.implementation.UserDetailServiceImpl;
 
+
+/*
+ * PushFacade serves as a facade for processing push operations of projects, directories, and files.
+ * It uses diferent services to create a push depending of the content of the request.
+ *
+ * Injects PushService to handle push requests containing project data, DirectoryService to handle
+ * push requests containing directory data, FileService to handle push requests containing file data,
+ * branch service to handle branch validation, UserProjectActivityService to handle user activity by
+ * project and branch, UserDetailServiceImpl to handle user validation, ResponseMapper to build responses
+ * for push data.
+ */
 @Component
 public class PushFacade {
 
@@ -64,6 +75,15 @@ public class PushFacade {
     this.responseMapper = responseMapper;
   }
 
+  /*
+   * Processes push based on content of pushDTO
+   * Determines the push type (project / directory / file) and passes
+   * data to appropriate handler method.
+   *
+   * @param pushDTO containing push request data
+   * @return pushResponseDTO containing push response data
+   * @throws InvalidPushException if request contains invalid data
+   */
   public PushResponseDTO processPush(PushDTO pushDTO) { 
     validatePushDTO(pushDTO); 
     if(pushDTO.getProjectFullDTO() != null) return handleProjectPush(pushDTO);
@@ -72,6 +92,13 @@ public class PushFacade {
     throw new InvalidPushException("Push request body is invalid, does not contain project/directory/file");
   }
 
+  /* TODO refactor method to separate branch logic in to a separate mehtod
+   *
+   * Handles push operation for request containing project data
+   *
+   * @param pushDTO containing push request data
+   * @return pushResponseDTO containing push response data
+   */
   private PushResponseDTO handleProjectPush(PushDTO pushDTO) {
     User user = userDetailServiceImpl.getUserByEmail(pushDTO.getOwnerEmail());
     Project project = projectService.createProjectFromPush(pushDTO.getProjectFullDTO(), user);
@@ -80,24 +107,53 @@ public class PushFacade {
     return responseMapper.toPushResponseFromProject(project);   
   }
 
-    private PushResponseDTO handleDirectoryPush(DirectoryFullDTO directoryDTO) {
+  /*
+   * Handles push operation for request containing directory data
+   *
+   * @param pushDTO containing push request data
+   * @return pushResponseDTO containing push response data
+   */
+  private PushResponseDTO handleDirectoryPush(DirectoryFullDTO directoryDTO) {
     Project project = projectService.getProjectById(directoryDTO.getProjectId());
     Directory directory = directoryService.createDirectoryFromPush(directoryDTO, project);
     return responseMapper.toPushResponseFromDirectory(directory);
    }
 
+  /*
+   * Handles push operation for request containing file data
+   *
+   * @param pushDTO containing push request data
+   * @return pushResponseDTO containing push response data
+   */
   private PushResponseDTO handleFilePush(FileDTO fileDTO) {
     Directory parentDirectory = directoryService.getDirectoryById(fileDTO.getDirectoryId());
     File file = fileService.createFileFromPush(fileDTO, parentDirectory);
     return responseMapper.toPushResponseFromFile(file);
   }
 
+  /*
+   * Validates data in push request by checking that request contains either project, directory, or file
+   *
+   * @param pushDTO containing push request data
+   * @throws InvalidPushException if project, directory, and file data is null
+   */
   private void validatePushDTO(PushDTO pushDTO) {
     if(pushDTO.getProjectFullDTO() == null && pushDTO.getDirectoryFullDTO() == null && pushDTO.getFileDTO() == null) {
       throw new InvalidPushException("Push request body is invalid, does not contain project/directory/file");
     } 
   }
 
+  /*
+   * TODO Does UserProjectActivity need renaming? UserProjectBranchActivity?
+   * TODO UserProjectActivity exception handling
+   * TODO Separate user fetching from user service to separate method and add user as input param?
+   *
+   * Records user activity by branch and project with UserProjectActivity
+   *
+   * @param pushDTO containing push request data
+   * @param project used in push operation
+   * @param branch used in push operation
+   */
   private void newUserProjectActivity(PushDTO pushDTO, Project project, Branch branch) {
     User user = userDetailServiceImpl.getUserByEmail(pushDTO.getOwnerEmail());
     
